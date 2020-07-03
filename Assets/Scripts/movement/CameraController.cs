5e522e4b3fb6aebf967ae442934b33d53d06fecd
ShreyanshDarshan <shreyanshdarshan1@gmail.com>
2020-07-03 16:41:42 +0530

    Add Camera Controls for no clip fly
    Control over camera for better view of the objects in the scene

---
+++ Assets/Scripts/movement/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float horizontalSensitivity = 1.0f;
+    public float verticalSensitivity = 1.0f;
+    public float movementSpeed = 0.01f;
+    public Transform playerbody;
+    float mouseX;
+    float mouseY;
+    float xRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * horizontalSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * verticalSensitivity * Time.deltaTime;
+
+        //xRotation -= Mathf.Lerp(xRotation, mousey);
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerbody.Rotate(Vector3.up * mouseX);
+
+        playerbody.Translate(playerbody.InverseTransformDirection(transform.forward) * Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime);
+        playerbody.Translate(playerbody.InverseTransformDirection(playerbody.right) * Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime);
+    }
+}
